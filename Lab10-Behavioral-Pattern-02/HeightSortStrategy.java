9c60348587ea87f5865aa902cee314b2b01e1c5f
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-11-02 16:12:27 +0700

    Add Lab10

---
+++ Lab10-Behavioral-Pattern-02/HeightSortStrategy.java
@@ -0,0 +1,12 @@
+package edu.parinya.softarchdesign.behavior2;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class HeightSortStrategy implements SortStrategy {
+    // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
+    @Override
+    public void customSort(List<Person> people) {
+        people.sort(Comparator.comparingInt(Person::getHeight));
+    }
+}
